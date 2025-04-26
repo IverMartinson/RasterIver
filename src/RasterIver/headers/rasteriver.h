815b649e1090199226a869b74b3ce11df8a0f6fd
IverMartinson <iver.m.martinson@gmail.com>
2025-04-26 16:19:36 -0400

    moved constants to header file & made them an enum

--- src/RasterIver/headers/rasteriver.h
+++ src/RasterIver/headers/rasteriver.h
@@ -4,12 +4,23 @@
 #include <stdint.h>
 
 typedef int RI_result;
+typedef int RI_flag;
 typedef uint32_t RI_uint;
 
-extern const RI_result RI_SUCCESS;
-extern const RI_result RI_ERROR;
-extern const RI_result RI_NOT_RUNNING;
-extern const RI_result RI_RUNNING;
+// RI_result
+typedef enum {
+    RI_ERROR = -1,
+    RI_SUCCESS = 0,
+    RI_NOT_RUNNING = -2,
+    RI_RUNNING = 1,
+};
+
+// RI_flag
+typedef enum {
+    RI_FLAG_DEBUG = 0,
+    RI_FLAG_DEBUG_VERBOSE = 1,
+    RI_FLAG_SHOW_Z_BUFFER = 2,
+};
 
 RI_result RI_Init();
 RI_result RI_Stop();
@@ -18,6 +29,6 @@ RI_result RI_RequestPolygons(int RI_PolygonsToRequest);
 RI_result RI_Tick();
 RI_result RI_SetBackground(RI_uint RI_BackgroundColor);
 RI_result RI_ShowZBuffer(int RI_ShowZBufferFlag);
-RI_result RI_SetDebugFlag(int RI_ShowDebugFlag);
+RI_result RI_SetFlag(RI_flag RI_FlagToSet, int RI_Value);
 
 #endif // RASTERIVER_H
