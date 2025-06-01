69dfd26b29cfda4f0c29c187498f18f32b418009
IverMartinson <iver.m.martinson@gmail.com>
2025-05-31 21:05:24 -0400

    it turned yellow

--- src/RasterIver/headers/master_kernel.h
+++ src/RasterIver/headers/master_kernel.h
@@ -257,9 +257,14 @@ float u_z2;\
             \
             break;}\
         case 3:{\
-        ux = w0 * u_x0 + w1 * u_x1 + w2 * u_x2;\
-                    uy = w0 * u_y0 + w1 * u_y1 + w2 * u_y2;\
-                    uz = w0 * u_z0 + w1 * u_z1 + w2 * u_z2;\
+            ux = w0 * u_x0 + w1 * u_x1 + w2 * u_x2;\
+            uy = w0 * u_y0 + w1 * u_y1 + w2 * u_y2;\
+            uz = w0 * u_z0 + w1 * u_z1 + w2 * u_z2;\
+            \
+            ux = clamp((ux * 0.5f + 0.5f) * 255.0f, 0.0f, 255.0f);\
+            uy = clamp((uy * 0.5f + 0.5f) * 255.0f, 0.0f, 255.0f);\
+            uz = clamp((uz * 0.5f + 0.5f) * 255.0f, 0.0f, 255.0f);\
+            \
             uchar r = (uchar)ux;\
             uchar g = (uchar)uy;\
             uchar b = (uchar)uz;\
@@ -271,7 +276,7 @@ float u_z2;\
             }\
             \
             frame_buffer[(height - id_y) * width + id_x] = 0xFF000000 | (r << 16) | (g << 8) | b;\
-\
+            \
             break;}\
         default:{\
             break;}\
